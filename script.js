f2d8202e5d59ac2a911bef6e2c6df7a4c25f9b73
markt <markdlt4@gmail.com>
2020-10-29 15:00:26 -0500

    Fixed Functions on password generator

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var upper = "ABCDEF"
 var lower = "abcdef"
 var num = "12345"
 var special = "!@#$%^&*()_"
-var numchar_input = 10;
+var numchar_input;
 var upper_confirm;
 var lower_confirm;
 var password_char = "ABCDEFabcdef1235"
@@ -15,33 +15,33 @@ function writePassword() {
     var passwordText = document.querySelector("#password");
 
 
-    numchar_input = parseInt(prompt("prompt"));
+    numchar_input = parseInt(prompt("Please choose a number between 8 and 128."));
+
     if (!numchar_input) {
         alert("no value")
-    } else if (numchar_input < 8 || numchar_input > 124) {
-        alert("not right amount")
-        numchar_input = parseInt(prompt("prompt"));
+    } else if (numchar_input < 8 || numchar_input > 128) {
+        alert("not the right amount")
+        //numchar_input = parseInt(prompt("Please choose a number between 8 and 128."));//
+        return;
     }
     else {
-        upper_confirm = confirm("upper")
-        lower_confirm = confirm("lower")
+        confirmQuestions()
     };
 
-    if (!upper_confirm && !lower_confirm) {
+    if (!upper_confirm && !lower_confirm && !numeric_confirm && !special_confirm) {
         alert("nothing picked")
-        upper_confirm = confirm("upper")
-        lower_confirm = confirm("lower")
+        return;
     }
-    else if (upper_confirm == true && lower_confirm == true) {
+    
+    else if (upper_confirm === true && lower_confirm === true) {
         password_char = password_char + upper + lower;
     }
-    else if (lower_confirm == true && upper_confirm == false) {
+    else if (lower_confirm === true && upper_confirm === false) {
         password_char = password_char + lower;
-    } else if (lower_confirm == false && upper_confirm == true) {
+    } else if (lower_confirm === false && upper_confirm === true) {
         password_char = password_char + upper;
     }
-
-
+    
     console.log(numchar_input);
     console.log(upper_confirm);
     console.log(lower_confirm);
@@ -51,7 +51,12 @@ function writePassword() {
     passwordText.value = password;
 
 }
-
+function confirmQuestions() {
+    upper_confirm = confirm("Would you like uppercase characters?")
+    lower_confirm = confirm("Would you like lowercase characters?")
+    numeric_confirm = confirm("Would you like numeric characters?")
+    special_confirm = confirm("Would you like special characters?")
+}
 
 
 // Add event listener to generate button
